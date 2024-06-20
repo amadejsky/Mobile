7a1380540f3f6cd3193b9c0faec71d3cf74afba1
amadejsky <adrian.madejski@vp.pl>
2024-06-20 11:41:44 +0200

    Wrapping widgets, gradient bg, code-formating

--- flutter_dart/first_app/lib/main.dart
+++ flutter_dart/first_app/lib/main.dart
@@ -1,6 +1,23 @@
+import 'package:flutter/material.dart';
 
-void main(){
-  runApp();
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 109, 33, 79),
+                Color.fromARGB(255, 24, 44, 97)
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World!'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
-
-
