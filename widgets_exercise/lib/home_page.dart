0fbe0437dc8f431f76db45b89f6dac7ff0216bf4
amadejsky <adrian.madejski@vp.pl>
2024-07-24 14:05:49 +0200

    LoadingScreen created, switching displayed widgets witch initState delay simulationg loading of application created

--- widgets_exercise/lib/home_page.dart
+++ widgets_exercise/lib/home_page.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:widgets_exercise/loading.dart';
 import 'package:widgets_exercise/player_screen.dart';
 
 class HomePage extends StatefulWidget{
@@ -13,11 +14,21 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage>{
+  bool _isLoading = true;
   @override
-  Widget build(context) {
-    return Center(
-      child: PlayerScreen(),
-      );
+  void initState(){
+    Future.delayed(const Duration(seconds: 3),(){
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(context){
+    return Scaffold(
+     body: _isLoading ? const Loading() : PlayerScreen(),
+    );
   }
 
 }
--- widgets_exercise/lib/main.dart
+++ widgets_exercise/lib/main.dart
@@ -4,6 +4,17 @@ import 'package:widgets_exercise/home_page.dart';
 
 void main(){
   runApp(
-    const HomePage()
+    const MyApp(),
   );
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(context){
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
 }
