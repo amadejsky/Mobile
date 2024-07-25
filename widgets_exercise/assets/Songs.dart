90b407f56aca4854cf3838b195869e811a842588
amadejsky <adrian.madejski@vp.pl>
2024-07-25 10:43:01 +0200

    Song model and 4 dummy example songs to make video player change state

---
+++ widgets_exercise/assets/Songs.dart
@@ -0,0 +1,9 @@
+
+import '../models/song_model.dart';
+
+const songs = [
+  SongModel('In This Together', 'Boil The Ocean'),
+  SongModel('Do I Wanna Know?', 'Arctic Monkeys'),
+  SongModel('Last Nite', 'The Strokes'),
+  SongModel('All Eyez On Me', '2Pac'),
+];
